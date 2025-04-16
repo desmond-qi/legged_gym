36cb42e96cdadc34106b24d4b604ad4da709bbe2
desmond-qi <qihao0624@foxmail.com>
2025-04-16 10:17:54 +0800

    [Update][*]增加tiangong模型[*]增加tiangong的跳跃训练

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -40,7 +40,8 @@ from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .jump_robot.jump_robot import JumpRobot
 from .jump_robot.jump_robot_config import JumpRobotCfg, JumpRobotCfgPPO
-
+from .tiangong.tiangong import Tiangong
+from .tiangong.tiangong_config import TiangongCfg, TiangongCfgPPO
 
 import os
 
@@ -52,3 +53,4 @@ task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPP
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
 task_registry.register( "jump_robot", JumpRobot, JumpRobotCfg(), JumpRobotCfgPPO() )
+task_registry.register( "tiangong", Tiangong, TiangongCfg(), TiangongCfgPPO() )
